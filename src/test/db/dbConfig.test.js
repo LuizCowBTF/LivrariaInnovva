a70351e1aa4fcd4be8945ad7ec4fb1ee6db3d27f
LuizCowBTF <luizcowbtf@gmail.com>
2024-02-28 16:46:19 -0300

    db

--- src/test/db/dbConfig.test.js
+++ src/test/db/dbConfig.test.js
@@ -4,8 +4,8 @@ import db from '../../db/dbconfig.js';
 describe('Testando configDB', () => {
   it('Teste de conexao com o banco de dados', async () => {
     const autorMock = {
-      nome: 'Raphael',
-      nacionalidade: 'Brasileiro',
+      nome: 'Luana',
+      nacionalidade: 'Brasileira',
       created_at: new Date().toISOString(),
       updated_at: new Date().toISOString(),
     };
@@ -15,5 +15,6 @@ describe('Testando configDB', () => {
         .where('id', retorno[0]))
       .then((autorSelecionado) => autorSelecionado[0]);
     expect(autorSalvo.nome).toBe(autorMock.nome);
+    await db('autores').where({ id: autorSalvo.id}).del();
   });
 });
